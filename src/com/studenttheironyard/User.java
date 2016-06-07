bd01d2f372c2b92404f7ae5e76876c26eea2453d
hoseasandstrom <Lakyn4ever>
2016-06-07 11:02:25 -0400

    Initial Setup

---
+++ src/com/studenttheironyard/User.java
@@ -0,0 +1,14 @@
+package com.studenttheironyard;
+
+/**
+ * Created by hoseasandstrom on 6/7/16.
+ */
+public class User {
+    String name;
+    String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
